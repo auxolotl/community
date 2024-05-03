8d4300a12e61c130624f53172ff1d18f47b7ce0c
Skyler Grey <minion@clicks.codes>
2024-05-03 23:23:15 +0000

    Add support for syncing forum groups
    The discourse API is rather confusing and documentation is lackluster,
    however with a little trial and effort and the pydiscourse library I have
    successfully made something that Works On My Discourse Instance:tm:.
    
    We can't directly use the update group endpoint, as this doesn't update
    group owners properly (see https://meta.discourse.org/t/cannot-update-group-owners-via-ruby-or-rest-api/98253),
    and as far as I know there is no scope for the group ownership endpoint
    so this change requires a token with full, unscoped administrator access.
    
    Sorry.
    
    As an alternative tactic, we may consider having a second "owner" group,
    however this would likely not let owners update some group settings. Then
    again, we probably don't want group owners to add new members or set
    joins open as those will be overriden by this script. A second "owner"
    group may also have the advantage of category specific moderation (see
    https://meta.discourse.org/t/category-group-review-moderation/116478)
    
    This still needs testing in some cases, in particular promoting/demoting
    multiple owners at once. The Discourse API is somewhat inconsistent so I
    cannot be certain that this is possible. Please add to the git notes
    (https://git-scm.com/docs/git-notes) once you have performed this testing

---
+++ sigmod.py
@@ -0,0 +1,72 @@
+import subprocess
+import json
+import pydiscourse
+import os
+
+sigs_eval_result = subprocess.run(["nix", "eval", "-f", "memberships.nix", "--json"], capture_output=True, text=True)
+sigs = json.loads(sigs_eval_result.stdout)
+
+forum_base_url = os.environ["DISCOURSE_URL"]
+forum_api_username = os.environ["DISCOURSE_API_USER"]
+forum_api_key = os.environ["DISCOURSE_API_KEY"]
+
+def extract_accounts(account_type, leaders, members):
+    leader_forum_accounts = set()
+    member_forum_accounts = set()
+
+    for user in leaders:
+        if account_type not in user:
+            continue
+        
+        leader_forum_accounts.add(user[account_type])
+        member_forum_accounts.add(user[account_type])
+
+    for user in members:
+        if account_type not in user:
+            continue
+
+        member_forum_accounts.add(user[account_type])
+
+    return { "leaders": leader_forum_accounts, "members": member_forum_accounts }
+
+def reconcile_forum_accounts():
+    forum_client = pydiscourse.DiscourseClient(api_key=forum_api_key, host=forum_base_url, api_username=forum_api_username)
+
+    for sig in sigs:
+        group_name = sig["sig"]["forum"]
+        group_id = forum_client.group(group_name)["group"]["id"]
+
+        should_be = extract_accounts("forum", sig["leaders"], sig["members"])
+        is_in = {
+            "members": set(map(lambda user: user["username"], forum_client.group_members(group_name))),
+            "leaders": set(map(lambda user: user["username"], forum_client.group_owners(group_name)))
+        }
+
+        to_remove_members = is_in["members"] - should_be["members"]
+        to_remove_leaders = is_in["leaders"] - should_be["leaders"] - to_remove_members
+        # No need to explicitly remove leadership if they are no longer in the group
+
+        to_add_leaders = should_be["leaders"] - is_in["leaders"]
+        to_add_members = should_be["members"] - is_in["members"] - to_add_leaders
+        # No need to explicitly add membership if they are being added as a leader
+
+        print(to_remove_members, to_remove_leaders, to_add_members, to_add_leaders)
+
+        if to_remove_members:
+            forum_client.delete_group_member(group_id, ",".join(to_remove_members))
+
+        if to_remove_leaders:
+            to_remove_leader_ids = []
+
+            for leader in to_remove_leaders:
+                to_remove_leader_ids.append(str(forum_client.user(leader)["id"]))
+
+            forum_client.delete_group_owner(group_id, ",".join(to_remove_leader_ids))
+
+        if to_add_members:
+            forum_client.add_group_members(group_id, to_add_members)
+
+        if to_add_leaders:
+            forum_client.add_group_owners(group_id, to_add_leaders)
+
+reconcile_forum_accounts()
